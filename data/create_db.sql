19e6f5374767bd7d93133fca3ed78d9f0b34e6e1
MitsukeZ <91833290+MitsukeZ@users.noreply.github.com>
2022-05-22 21:22:11 +0200

    la bado tavu

--- data/create_db.sql
+++ data/create_db.sql
@@ -1,5 +1,38 @@
-CREATE DATABASE reviews;
+CREATE DATABASE forum;
 
+USE forum;
+
+
+CREATE TABLE Users (
+    idUser INT  PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    pseudo  VARCHAR(60),
+    hashMdp VARCHAR(255),
+    UNIQUE(pseudo)
+);
+
+CREATE TABLE Topic (
+    idTopic INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    titre   VARCHAR(200),
+    contenu TEXT,
+    idUser  INT REFERENCES Users(idUser)
+);
+
+CREATE TABLE Reponse (
+    idReponse INT PRIMARY KEY AUTO_INCREMENT,
+    idTopic INT REFERENCES Topic(idTopic),
+    idUser  INT REFERENCES Users(idUser),
+    contenu TEXT
+);
+
+INSERT INTO Topic (titre, contenu)
+VALUES ("Topic de test", "Floris est raciste");
+
+INSERT INTO Reponse (idTopic, contenu)
+VALUES (1, "t'es guez bro");
+
+GRANT ALL ON forum.* to review_site@localhost IDENTIFIED BY 'JxSLRkdutW';
+
+/*
 USE reviews;
 
 CREATE TABLE user_review (
@@ -13,3 +46,4 @@ INSERT INTO user_review (reviewer_name, star_rating, details) VALUES ('Ben', '5'
 INSERT INTO user_review (reviewer_name, star_rating, details) VALUES ('Leslie', '1', 'Calzones are the worst.');
 
 GRANT ALL ON reviews.* to review_site@localhost IDENTIFIED BY 'JxSLRkdutW';
+*/
