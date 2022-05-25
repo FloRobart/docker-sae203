7584cd52aeaea2ad9e5f392101dc21d3c30e8beb
MitsukeZ <91833290+MitsukeZ@users.noreply.github.com>
2022-05-25 09:08:56 +0200

    Delete inserts.sql

--- data/inserts.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO User (pseudo, hashMdp)
-VALUES ("admin"    ,"$2y$10$78BZz8epoRwHYhjLqMFeTeKS4rHHWeBaFjGftRSRCD4eipCT8bkfO"),
-       ("Forumeur" ,"$2y$10$1LdBFnTFxkOsXFiKVLdi6uavifL7ed5Ym7yIgwnTcqT18kOe.dbc6"),
-       ("34alain34","$2y$10$kwe6nhnrkI4URvW42fapve95bGlpYe5VGkAKAYvYFWi7PSAD0/bzK"),
-       ("FloRobart","$2y$10$hdrPunBCejxhg8c30EQmTuGccUF.HYVyLUBEieSypnG04uxVDmkHC"),
-       ("MitsukeZ" ,"$2y$10$OfAN5wI0I3BO7h1Iv6Wn9.MzogMGBWmKJYyBJlqBl3J.dkU/tF0BK");
